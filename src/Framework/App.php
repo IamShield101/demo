91eec8ecffc8784a75abfd4c9e56c50dd0f4512d
Iamshield101 <cyclonesshield@gmail.com>
2024-01-29 18:27:11 +0100

    ghjkl

---
+++ src/Framework/App.php
@@ -0,0 +1,16 @@
+<?php
+
+    declare (strict_types=1);
+
+    namespace Framework;
+
+    Class App
+    {
+      public function run()
+      {
+        echo "This app is working";
+
+      }
+
+
+    }
